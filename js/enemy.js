cb4d0a0a5cd949d364ff62a16e5766bee95765f1
elizab238 <lizwanyoike20@gmail.com>
2021-01-26 22:55:39 +0300

    added all files

---
+++ js/enemy.js
@@ -0,0 +1,10 @@
+let enemy;
+
+function player (enemytype, health, manner, health, agility, speed){
+    this.enemytype;
+    this.health;
+    this.manner;
+    this.health;
+    this.agility;
+    this.speed;
+}
---
+++ js/player.js
@@ -0,0 +1,10 @@
+let player;
+
+function player (classtype, health, manner, health, agility, speed){
+    this.classtype;
+    this.health;
+    this.manner;
+    this.health;
+    this.agility;
+    this.speed;
+}
